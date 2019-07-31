d9caf4a75fa653bfeb9c8ff798acca28514c627b
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-07-31 17:07:28 +0200

    Update raise_marque.rb

--- plugins/custom/raise_marque.rb
+++ plugins/custom/raise_marque.rb
@@ -3,20 +3,32 @@ module AresMUSH
     class RaiseMarqueCmd
       include CommandHandler
       
-      attr_accessor :marque
-   
+      attr_accessor :name, :num
+
+      def parse_args
+        args = cmd.parse_args( /(?<name>[^\=]+)\/(?<num>.+)/)
+        self.name = titlecase_arg(args.name)
+        self.num = trim_arg(args.num)
+      end
+
+      def required_args
+        [ self.name, self.num ]
+      end      
+      
       def handle
-        if !enactor.has_role?("admin")
-          client.emit_failure "Only staff are allowed to trigger this command."
-          return nil
-        elsif target.ranks_rank != "Adept"
-          client.emit_failure "Only adept characters can have their marque raised!"
-          return nil
-        else 
-          self.marque = target.marque + value
-          target.update(marque: self.marque < 100 ? self.marque : 100)
-          client.emit_success "#{target.name}'s marque has been raised to #{target.marque}."
-          return true
+        ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
+          if !enactor.has_role?("admin")
+            client.emit_failure "Only staff are allowed to trigger this command."
+            return nil
+          elsif model.ranks_rank != "Adept"
+            client.emit_failure "Only adept characters can have their marque raised!"
+            return nil
+          else 
+            self.marque = model.marque + self.num
+            model.update(marque: self.marque < 100 ? self.marque : 100)
+            client.emit_success "#{model.name}'s marque has been raised to #{model.marque} percent."
+            return true
+          end
         end
       end  
     end
