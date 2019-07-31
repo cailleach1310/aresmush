372a4a1278fa1cd4ce5c1ddd2ccea969d1a8aea1
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-07-31 08:50:31 +0200

    Update init_marque.rb

--- plugins/custom/init_marque.rb
+++ plugins/custom/init_marque.rb
@@ -6,16 +6,16 @@ module AresMUSH
       attr_accessor :marque
    
       def handle
-        if Chargen.check_chargen_locked(enactor)
+        if !enactor.has_role?("admin")
           client.emit_failure "Only staff are allowed to trigger this command."
           return nil
-        elsif enactor.ranks_rank != "Novice"
+        elsif target.ranks_rank != "Novice"
           client.emit_failure "Initialising marque is only possible for a novice after their debut!"
           return nil
         else 
-          enactor.update(marque: 0)
-          enactor.update(rank: "Adept")
-          client.emit_success "The marque has been set to zero! #{enactor.name} now has new status of adept."
+          target.update(marque: 0)
+          target.update(rank: "Adept")
+          client.emit_success "The marque has been set to zero! #{target.name} now has new status of adept."
           return true
         end
       end  
