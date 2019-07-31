5f0cea500c8320ba1772510990c9ddfc5baf1f8c
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-07-31 09:02:45 +0200

    Create raise_marque.rb

---
+++ plugins/custom/raise_marque.rb
@@ -0,0 +1,24 @@
+module AresMUSH
+  module Custom
+    class RaiseMarqueCmd
+      include CommandHandler
+      
+      attr_accessor :marque
+   
+      def handle
+        if !enactor.has_role?("admin")
+          client.emit_failure "Only staff are allowed to trigger this command."
+          return nil
+        elsif target.ranks_rank != "Adept"
+          client.emit_failure "Only adept characters can have their marque raised!"
+          return nil
+        else 
+          self.marque = target.marque + value
+          target.update(marque: self.marque)
+          client.emit_success "#{target.name}'s marque has been raised to #{target.marque}."
+          return true
+        end
+      end  
+    end
+  end
+end
