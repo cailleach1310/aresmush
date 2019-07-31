80a8b6c384598a786fa926414a5387451f97eabe
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-07-31 10:17:42 +0200

    Update init_marque.rb

--- plugins/custom/init_marque.rb
+++ plugins/custom/init_marque.rb
@@ -3,19 +3,31 @@ module AresMUSH
     class InitMarqueCmd
       include CommandHandler
       
-      attr_accessor :marque
-   
+      attr_accessor :name, :num
+
+      
+      def parse_args
+        args = cmd.parse_args( /(?<name>[^\=]+)\/(?<num>.+)/)
+        self.name = titlecase_arg(args.name)
+        self.num = trim_arg(args.num)
+      end
+      
+      def required_args
+        [ self.name, self.num ]
+      end
+      
       def handle
-        if !enactor.has_role?("admin")
-          client.emit_failure "Only staff are allowed to trigger this command."
-          return nil
-        elsif target.ranks_rank != "Novice"
-          client.emit_failure "Initialising marque is only possible for a novice after their debut!"
-          return nil
-        else 
-          target.update(marque: 0)
-          target.update(rank: "Adept")
-          client.emit_success "The marque has been set to zero! #{target.name} now has new status of adept."
+          ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
+          if !enactor.has_role?("admin")
+            client.emit_failure "Only staff are allowed to trigger this command."
+            return nil
+          elsif model.ranks_rank != "Novice"
+            client.emit_failure "Initialising marque is only possible for a novice after their debut!"
+            return nil
+         else 
+            model.update(marque: 0)
+            model.update(ranks_rank: "Adept")
+            client.emit_success "The marque has been set to zero! #{model.name} now has new status of adept."
           return true
         end
       end  
