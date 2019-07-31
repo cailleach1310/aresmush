bcd2442a6083dd8689f2d536e8dc6163aeab20b6
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-07-31 11:10:59 +0200

    Update acknowledge_marque.rb

--- plugins/custom/acknowledge_marque.rb
+++ plugins/custom/acknowledge_marque.rb
@@ -3,20 +3,22 @@ module AresMUSH
     class AcknMarqueCmd
       include CommandHandler
       
-      attr_accessor :marque
+      attr_accessor :name
    
       def handle
-        if !enactor.has_role?("admin")
-          client.emit_failure "Only staff are allowed to trigger this command."
-          return nil
-        elsif target.ranks_rank != "Adept"
-          client.emit_failure "Only adept characters can have their marque acknowledged!"
-          return nil
-        else 
-          target.update(marque: nil)
-          target.update(rank: "Courtesan")
-          client.emit_success "The marque has been acknowledged! #{target.name} now has new status of fully marqued courtesan."
-          return true
+        ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
+          if !enactor.has_role?("admin")
+            client.emit_failure "Only staff are allowed to trigger this command."
+            return nil
+          elsif model.ranks_rank != "Adept"
+            client.emit_failure "Only adept characters can have their marque acknowledged!"
+            return nil
+          else 
+            model.update(marque: nil)
+            model.update(ranks_rank: "Courtesan")
+            client.emit_success "The marque has been acknowledged! #{model.name} now has new status of fully marqued courtesan."
+            return true
+          end
         end
       end  
     end
