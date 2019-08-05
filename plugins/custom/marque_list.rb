ba77b1c3c4aca340b432baf6b0d9e8a24f9bf157
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-08-05 20:14:05 +0200

    Create marque_list.rb

---
+++ plugins/custom/marque_list.rb
@@ -0,0 +1,15 @@
+module AresMUSH
+  module Custom
+    class ListMarqueCmd
+      include CommandHandler
+      
+      def handle
+        <%= header %>
+        client.emit("%xcList of Adepts%xn:%r%r")
+        Character.all.select { |c| c.ranks_rank == 'Adept' }.map 
+          { |c| client.emit("* #{c.name} -- Marque at #{c.marque}% -- last on: #{c.last_on}") }
+        <%= footer %>
+      end
+    end  
+  end   
+end   
