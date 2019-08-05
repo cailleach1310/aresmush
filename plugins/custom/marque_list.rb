640c9c83181fa08d6060a71709dd928bd5f93021
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-08-05 21:23:01 +0200

    Update marque_list.rb

--- plugins/custom/marque_list.rb
+++ plugins/custom/marque_list.rb
@@ -6,10 +6,10 @@ module AresMUSH
       def handle
 #        Character.all.select { |model| model.ranks_rank == 'Adept' }.map 
 #          client.emit("* #{c.name} -- Marque at #{c.marque}'%' -- last on: #{c.last_on}") }ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-          template = BorderedDisplayTemplate.new model.name model.marque model.last_on, "List of Adepts"
-          client.emit template.render
-        end
-      end
+#          template = BorderedDisplayTemplate.new model.name model.marque model.last_on, "List of Adepts"
+#          client.emit template.render
+#        end
+#      end
 #        <%= header %>
 #        client.emit("%xcList of Adepts%xn:%r%r")
 #        Character.all.select { |c| c.ranks_rank == 'Adept' }.map { |c| client.emit("* #{c.name} -- Marque at #{c.marque}% -- last on: #{c.last_on}") }
