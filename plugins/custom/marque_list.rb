0cb4f103c586562c2d8124be1839a65d105e31d3
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-08-05 20:47:51 +0200

    Update marque_list.rb

--- plugins/custom/marque_list.rb
+++ plugins/custom/marque_list.rb
@@ -5,9 +5,9 @@ module AresMUSH
       
       def handle
 #        <%= header %>
-        client.emit("%xcList of Adepts%xn:%r%r")
-        Character.all.select { |c| c.ranks_rank == 'Adept' }.map { |c| client.emit("* #{c.name} -- Marque at #{c.marque}% -- last on: #{c.last_on}") }
-          client.emit("* #{c.name} -- Marque at #{c.marque}'%' -- last on: #{c.last_on}") } end 
+#        client.emit("%xcList of Adepts%xn:%r%r")
+#        Character.all.select { |c| c.ranks_rank == 'Adept' }.map { |c| client.emit("* #{c.name} -- Marque at #{c.marque}% -- last on: #{c.last_on}") }
+#          client.emit("* #{c.name} -- Marque at #{c.marque}'%' -- last on: #{c.last_on}") } end 
 #        <%= footer %>
       end
     end  
