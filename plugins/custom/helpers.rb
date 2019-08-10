7b53b0c6134bfeaa4e9dddce2fc41c583ec67521
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-08-10 14:57:33 +0200

    Update helpers.rb

--- plugins/custom/helpers.rb
+++ plugins/custom/helpers.rb
@@ -1,8 +1,10 @@
 module AresMUSH
   module Custom
     def self.do_marque_raise(target, amount)
-      new_marque = target.marque.to_i + amount
-      target.update(marque: new_marque < 100 ? new_marque : 100)
+      if !enactor.has_role?("admin")
+       new_marque = target.marque.to_i + amount
+       target.update(marque: new_marque < 100 ? new_marque : 100)
+      end  
     end
   end
 end
