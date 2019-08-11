400fdc088622235ffce5d6ebe11d929ea5b8bb3e
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2019-08-11 17:10:23 +0200

    Update helpers.rb
    adding check can_view_goals

--- plugins/custom/helpers.rb
+++ plugins/custom/helpers.rb
@@ -1,8 +1,19 @@
 module AresMUSH
   module Custom
+
     def self.do_marque_raise(target, amount)
       new_marque = target.marque.to_i + amount
       target.update(marque: new_marque < 100 ? new_marque : 100)
+    end
+
+    def self.check_can_view_goals?(target, viewer)
+      if target.name == viewer.name
+        return nil
+      elsif Character.has_permission?(viewer, 'view_bgs')
+        return nil
+      else
+        return false
+      end
     end
   end
 end
