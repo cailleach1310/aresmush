b181a2f24f1072c88da0d8d6043f2432f0376186
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-03-30 20:00:46 +0200

    marque/list fix, final

--- plugins/custom/marque_list.rb
+++ plugins/custom/marque_list.rb
@@ -3,20 +3,18 @@ module AresMUSH
     class ListMarqueCmd
       include CommandHandler
       
-      def handle
-        ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
-        if !enactor.has_role?("admin")
-          client.emit_failure "Only staff are allowed to trigger this command."
-          return nil
-        else
-          adept_chars = Character.all.select { |char| char.ranks_rank == 'Adept' }
-          template = MarqueTemplate.new adept_chars
-          client.emit template.render
-        end
-      end
+      def check_can_view
+        return nil if enactor.has_role?("admin")
+        return "You are not allowed to use this command."
+      end	
 
+      def handle
+        adept_chars = Character.all.select { |char| char.ranks_rank == 'Adept' }
+
+        template = MarqueTemplate.new adept_chars
+	client.emit template.render
+      end 
     end  
   end
 end
-
 
