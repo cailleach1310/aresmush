57d4eff0385dde701b24d5a1c1515897085934e0
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-11 08:51:07 +0200

    Update custom_app_review.rb
    Adding custom checks for scion and marque.

--- plugins/chargen/custom_app_review.rb
+++ plugins/chargen/custom_app_review.rb
@@ -3,22 +3,30 @@ module AresMUSH
     def self.custom_app_review(char)
             
       # If you don't want to have any custom app review steps, return nil
-      return nil
+      # return nil
             
       # Otherwise, return a message to display.  Here's an example of how to 
       # give an alert if the character has chosen an invalid position for their 
       # faction.
       #
-      #  faction = char.group("Faction")
-      #  position = char.group("Position")
-      #  
-      #  if (position == "Knight" && faction != "Noble")
-      #    msg = "%xrOnly nobles can be knights.%xn"
-      #  else
-      #    msg = t('chargen.ok')
-      #  end
-      #
-      #  return Chargen.format_review_status "Checking groups.", msg
+      rank = char.ranks_rank
+      scion = (char.fs3_advantages.find(name: "Scion Power").first)
+      
+      msg = ""
+      
+      if (rank == "Adept" && !(char.marque))
+         msg = "%xrAdept concepts need to set their marque!%xn"
+      end
+      
+      if (scion && !(char.scion))
+         msg = msg + "%xrScions need to set their scion power!%xn"
+      end
+      
+      If (msg == "")
+         msg = t('chargen.ok')
+      end
+
+      return Chargen.format_review_status "Checking custom settings.", msg
       #
       # You can also use other built-in chargen status messages, like t('chargen.not_set').  
       # See https://www.aresmush.com/tutorials/config/chargen.html for details.
