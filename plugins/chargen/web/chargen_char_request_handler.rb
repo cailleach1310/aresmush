a546810edf419a8577137b24552dcc97db8fd8fc
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-16 21:26:37 +0200

    Update chargen_char_request_handler.rb

--- plugins/chargen/web/chargen_char_request_handler.rb
+++ plugins/chargen/web/chargen_char_request_handler.rb
@@ -60,6 +60,7 @@ module AresMUSH
         end
           
         hooks = Website.format_input_for_html(char.rp_hooks)
+        char_goals = Website.format_input_for_html(char.goals)
         
         {
           id: char.id,
@@ -69,7 +70,7 @@ module AresMUSH
           groups: groups,
           background: Website.format_input_for_html(char.background),
           rp_hooks: hooks,
-          goals: goals,
+          goals: char_goals,
           profile_image: char.profile_image,
           desc: Website.format_input_for_html(char.description),
           shortdesc: Website.format_input_for_html(char.shortdesc),
