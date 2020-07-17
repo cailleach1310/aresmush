ea5b7993034bec591cbcb32a576f2c2cf3fb98db
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-17 16:40:01 +0200

    Update helpers.rb

--- plugins/custom/helpers.rb
+++ plugins/custom/helpers.rb
@@ -18,17 +18,7 @@ module AresMUSH
       else
         return false
       end
-    end
-
-    def self.has_scion_set?(char)
-      if !char.scion
-        return false
-      else
-#        return { {{self.scions}}.member?(char.scion) }
-        return true
-      end
-    end
-  
+    end  
   
   end
 end
