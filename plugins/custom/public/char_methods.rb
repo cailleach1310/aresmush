39a0ec7344b805b28d1720480f6b4da7c1af2ecb
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-19 10:21:31 +0200

    Create char_methods.rb

---
+++ plugins/custom/public/char_methods.rb
@@ -0,0 +1,21 @@
+module AresMUSH
+  class Character
+
+    def self.has_scion_set?
+      if !self.scion
+        return false
+      else
+        return { Module::Custom.scions.member?(self.scion) }
+      end
+    end
+
+    def self.is_adept?
+      if !self.marque
+        return false
+      else
+        return true
+      end
+    end
+
+ end
+end
