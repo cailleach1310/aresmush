32b7d52f0863e2e6ad57a01f48415b84ad25663f
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-19 12:03:04 +0200

    Update scion_char.rb

--- plugins/custom/scion_char.rb
+++ plugins/custom/scion_char.rb
@@ -2,14 +2,5 @@ module AresMUSH
   class Character
     attribute :scion
 #    attribute :sp
-
-    def self.has_scion_set?
-      if !self.scion
-        return false
-      else
-        return { Module::Custom.scions.member?(self.scion) }
-      end
-    end
-
   end
 end
