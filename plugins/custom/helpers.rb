8b0680bfc74918d9c4b254c2fa8592388246830e
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-19 12:27:12 +0200

    Update helpers.rb

--- plugins/custom/helpers.rb
+++ plugins/custom/helpers.rb
@@ -1,18 +1,6 @@
 module AresMUSH
   module Custom
 
-    def self.scions
-    	return Global.read_config("custom", "scion_list")
-  	end
-
-    def self.has_scion_set?(char)
-      if !char.scion
-        return false
-      else
-        return { self.scions.member?(char.scion) }
-      end
-    end
-
     def self.do_marque_raise(target, amount)
       new_marque = target.marque.to_i + amount
       target.update(marque: new_marque < 100 ? new_marque : 100)
@@ -28,12 +16,5 @@ module AresMUSH
       end
     end  
 
-    def self.has_scion_set?(char)
-      if !char.scion
-        return false
-      else
-        return { {{self.scions}}.member?(char.scion) }
-      end
-    end  
   end
 end
