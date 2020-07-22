91168ce04e2bb20ed8b893f41740fd29677e3b32
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-22 16:55:28 +0200

    Update custom_api.rb

--- plugins/custom/public/custom_api.rb
+++ plugins/custom/public/custom_api.rb
@@ -1,18 +1,18 @@
 module AresMUSH
   module Custom
 
-    def self.scions
-    	return Global.read_config("custom", "scion_list")
-  	end
-
-    def self.has_scion_set?(char)
-      if !char.scion
-        return false
-      else
-        return { Custom.scions.member?(char.scion) }
-      end
-    end
-
+#    def self.scions
+#    	return Global.read_config("custom", "scion_list")
+#  	end
+#
+#    def self.has_scion_set?(char)
+#      if !char.scion
+#        return false
+#      else
+#        return { Custom.scions.member?(char.scion) }
+#      end
+#    end
+#
     def self.is_adept?(char)
        if (char.ranks_rank == "Adept")
          return true
