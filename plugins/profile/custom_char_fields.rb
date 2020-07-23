c7ef7b999a9771345f4861042788f9bdb15e3268
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-23 13:22:15 +0200

    Update custom_char_fields.rb

--- plugins/profile/custom_char_fields.rb
+++ plugins/profile/custom_char_fields.rb
@@ -18,8 +18,6 @@ module AresMUSH
       # Return a hash of custom fields formatted for editing in chargen
       # Example: return { goals: Website.format_input_for_html(char.goals) }
       def self.get_fields_for_chargen(char)
-#        return { marque: Website.format_input_for_html(char.marque),
-#          scion: Website.format_input_for_html(char.scion) }
          return { marque: Website.format_input_for_html(char.marque) }
      end
       
@@ -35,7 +33,6 @@ module AresMUSH
       def self.save_fields_from_chargen(char, chargen_data)
 #        char.update(goals: chargen_data[:custom][:goals])
         char.update(marque: chargen_data[:custom][:marque])
-#        char.update(scion: chargen_data[:custom][:scion])
         return []
       end
       
