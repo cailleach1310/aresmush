2a4671ee332684fb88e82cc5b01c1c31f4712937
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-24 09:07:09 +0200

    Update custom_approval.rb

--- plugins/chargen/custom_approval.rb
+++ plugins/chargen/custom_approval.rb
@@ -7,13 +7,12 @@ module AresMUSH
       # Otherwise, do what you need to do.  Here's an example of how to add
       # someone to a role based on their faction:
       #
-      #  faction = char.group("Faction")
-      #  role = Role.find_by_name(faction)
-      #
-      #  if (role)
-      #    char.roles.add role
-      #  end
-      #
+       faction = char.group("Faction")
+       role = Role.find_by_name(faction)
+      
+       if (role)
+         char.roles.add role
+       end
       # See https://www.aresmush.com/tutorials/config/chargen.html for details.
     end
   end
