5d0bf511c69060049855c3a1361a438f5c5e8755
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-25 08:27:14 +0200

    Update custom_char_fields.rb

--- plugins/profile/custom_char_fields.rb
+++ plugins/profile/custom_char_fields.rb
@@ -7,35 +7,36 @@ module AresMUSH
       # Example: return { goals: Website.format_markdown_for_html(char.goals) }
       def self.get_fields_for_viewing(char, viewer)
 #        return { goals: Website.format_markdown_for_html(char.goals) }
-        return { marque: Website.format_markdown_for_html(char.marque) }
-connections: Website.format_markdown_for_html(char.connections) }
+        return { goals: Website.format_markdown_for_html(char.goals),
+          marque: Website.format_markdown_for_html(char.marque),
+          connections: Website.format_markdown_for_html(char.connections) }
       end
     
       # Return a hash of custom fields formatted for editing in the profile editor
       # Example: return { goals: Website.format_input_for_html(char.goals) }
       def self.get_fields_for_editing(char, viewer)
-#        return { goals: Website.format_input_for_html(char.goals) }
-#        return { connections: Website.format_input_for_html(char.connections) }
+        return { goals: Website.format_input_for_html(char.goals) }
       end
 
       # Return a hash of custom fields formatted for editing in chargen
       # Example: return { goals: Website.format_input_for_html(char.goals) }
       def self.get_fields_for_chargen(char)
-         return { marque: Website.format_input_for_html(char.marque) ,
+         return { goals: Website.format_input_for_html(char.goals),
+           marque: Website.format_input_for_html(char.marque),
            connections: Website.format_input_for_html(char.connections) }
      end
       
       # Custom fields will be in char_data[:custom]
       # Example: char.update(goals: char_data[:custom][:goals])
       def self.save_fields_from_profile_edit(char, char_data)
- #        char.update(goals: char_data[:custom][:goals])        
+         char.update(goals: char_data[:custom][:goals])        
       end
       
       # Save fields and return an array of any error messages.
       # Note Custom fields will be in chargen_data[:custom]
       # Example: char.update(goals: chargen_data[:custom][:goals])
       def self.save_fields_from_chargen(char, chargen_data)
-#        char.update(goals: chargen_data[:custom][:goals])
+        char.update(goals: chargen_data[:custom][:goals])
         char.update(marque: chargen_data[:custom][:marque])
         char.update(connections: chargen_data[:custom][:connections])
         return []
