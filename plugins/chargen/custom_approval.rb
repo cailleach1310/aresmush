63a0d06cd5455b75866ca14399ba526aacb2dcd3
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-28 22:58:13 +0200

    Update custom_approval.rb

--- plugins/chargen/custom_approval.rb
+++ plugins/chargen/custom_approval.rb
@@ -2,11 +2,6 @@ module AresMUSH
   module Chargen
     def self.custom_approval(char)
             
-      # If you don't want to have any custom approval steps, just leave this blank.
-      
-      # Otherwise, do what you need to do.  Here's an example of how to add
-      # someone to a role based on their faction:
-      #
        faction = char.group("faction")
        role = Role.find_one_by_name(faction)
       
@@ -20,9 +15,22 @@ module AresMUSH
        if ((faction != "Commoner") && (faction != "Clergy"))
           tag_list.push(char.group("house"))
        end
+      # ^^ tags for houses may have to be tackled in a function (salons, etc)
        tags = (tag_list || []).map { |t| t.downcase }.select { |t| !t.blank? }
        char.update(profile_tags: tags)
-       # See https://www.aresmush.com/tutorials/config/chargen.html for details.
+
+      # auto-joining to the faction channel: still tbd
+      standard_channel = {
+         "Courtesan" => "Courtesan"
+         "Noble" => "Nobility"
+         "Commoner" => "Commoner"
+         "Clergy" => "Clergy"
+      }
+
+      Channels.add_to_channels(client, char, standard_channel[faction])
+      if (client)
+          client.emit_success t('channels.channel_command_hint')
+      end
     end
   end
 end
