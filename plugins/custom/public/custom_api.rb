779ff7b3f3e1d5c886e6b9e39429a9cb80e63bfb
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-29 12:48:56 +0200

    Update custom_api.rb

--- plugins/custom/public/custom_api.rb
+++ plugins/custom/public/custom_api.rb
@@ -1,14 +1,5 @@
  module AresMUSH
   module Custom
-
-  def self.standard_channel(char)
-    faction = char.group("faction")
-    if (faction == "Noble")
-      return "Nobility"
-    else
-      return faction
-    end
-  end
       
   def self.is_adept?(char)
     return { (char.ranks_rank == "Adept") }
