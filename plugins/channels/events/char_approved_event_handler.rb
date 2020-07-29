523991f4bee322d4bf8b113bd1e3d00f9ede3ffe
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-07-29 11:36:05 +0200

    Update char_approved_event_handler.rb

--- plugins/channels/events/char_approved_event_handler.rb
+++ plugins/channels/events/char_approved_event_handler.rb
@@ -6,7 +6,7 @@ module AresMUSH
         char = Character[event.char_id]
                 
         channels = Global.read_config("channels", "approved_channels")  
-        channels.push(Custom.standard_channel(char.groups("faction"))
+        channels.push(Custom.standard_channel(char))
         Channels.add_to_channels(client, char, channels)
         
         if (client)
