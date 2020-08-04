969c014915a42e1182efed122a8306256048befb
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-04 16:59:17 +0200

    Update custom_api.rb

--- plugins/custom/public/custom_api.rb
+++ plugins/custom/public/custom_api.rb
@@ -4,3 +4,10 @@
   def self.is_adept?(char)
     return { (char.ranks_rank == "Adept") }
   end
+
+  def self.set_connections?(char)
+    return { (char.fs3_advantages.find(name: "Connections").first) }
+  end
+
+  end
+ end
