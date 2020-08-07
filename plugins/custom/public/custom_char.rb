f953f2e14af05b99922cb8e6228a1f960de8535d
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2020-08-07 17:15:16 +0200

    Update custom_char.rb

--- plugins/custom/public/custom_char.rb
+++ plugins/custom/public/custom_char.rb
@@ -1,15 +1,15 @@
 module AresMUSH
-  module Custom
-  class Character
+ class Character
+#   attribute :is_adept, :type => DataType::Boolean
+#   attribute :set_connections, :type => DataType::Boolean
    
   def is_adept
-    return { (self.ranks_rank == "Adept") }
+    self.ranks_rank == "Adept"
   end
 
   def set_connections
-    return { (self.fs3_advantages.find(name: "Connections").first) }
+    self.fs3_advantages.find(name: "Connections").first
   end
 
-  end
  end
- end
+end
