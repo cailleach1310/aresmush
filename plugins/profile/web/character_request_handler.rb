fac10aa21c57744f966b2c2967c927f66dd8c355
cailleach1310 <37542686+cailleach1310@users.noreply.github.com>
2021-02-22 22:30:22 +0100

    Update character_request_handler.rb

--- plugins/profile/web/character_request_handler.rb
+++ plugins/profile/web/character_request_handler.rb
@@ -139,7 +139,6 @@ module AresMUSH
           
           roster: self.build_roster_info(char),
           idle_notes: char.idle_notes ? Website.format_markdown_for_html(char.idle_notes) : nil,
-          custom: show_background ? CustomCharFields.get_fields_for_viewing(char, enactor) : nil,
           show_notes: char == enactor || Utils.can_manage_notes?(enactor),
           siteinfo: siteinfo,
           roles: roles
